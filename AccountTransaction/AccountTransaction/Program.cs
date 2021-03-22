4702320f067cea854708e626a6cb365d1eb71cee
Mushfiqur Abir <mushfiqur.abir@gmail.com>
2021-03-22 14:01:02 +0600

    Todays Lab Task

---
+++ AccountTransaction/AccountTransaction/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountTransaction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new Account("Abir","1234",50000);
+            Account a2 = new Account("Sabir","5678",10000);
+            a1.showInfo();
+            a2.showInfo();
+            a1.Withdraw(1000);
+            a1.Deposit(10000);
+            a1.Transfer(a2, 5000);
+            a1.showInfo();
+            a2.showInfo();
+            Transaction t1 = new Transaction(a1,a2,7000,"Business perpose");
+            t1.showInfo();
+        }
+    }
+}
---
+++ AccountTransaction/AccountTransaction/Transaction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountTransaction
+{
+    class Transaction:Account
+    {
+        public Account sender { get; set; }
+        public Account receiver { get; set; }
+        public int amount { get; set; }
+        public string additionalInfo { get; set; }
+
+        public Transaction()
+        {
+            Console.WriteLine("Deafult Transaction.");
+        }
+        public Transaction(Account sender, Account receiver, int amount, string additionalInfo)
+        {
+            this.sender = sender;
+            this.receiver = receiver;
+            this.amount = amount;
+            this.additionalInfo = additionalInfo;
+        }
+
+        override public void showInfo()
+        {
+            Console.WriteLine("Sender is: " + sender.AccName);
+            Console.WriteLine("Receiver is: " + receiver.AccName);
+            Console.WriteLine("Amount is: " + amount);
+            Console.WriteLine("Additional Info: " + additionalInfo);
+        }
+    }
+}
